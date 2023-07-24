9876787994d3af6efe9bbfc0c0792986b31a5604
rickytree <tronionio21@gmail.com>
2023-07-24 07:40:53 +0200

    Configuring the Network

--- tokens/hardhat.config.js
+++ tokens/hardhat.config.js
@@ -1,6 +1,15 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-toolbox');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: '0.8.19',
+  networks: {
+    sepolia: {
+      url: process.env.QUICKNODE_RPC_URL,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
